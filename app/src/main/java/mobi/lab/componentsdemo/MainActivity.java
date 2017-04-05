a5492a9eafb6ee2e49609a8340c3801874c3c769
lauris <lauris.kruusamae@lab.mobi>
2017-04-05 22:27:14 +0300

    Add base navigation logic and an acitivity for displaying our BoundedView example

--- app/src/main/java/mobi/lab/componentsdemo/MainActivity.java
+++ app/src/main/java/mobi/lab/componentsdemo/MainActivity.java
@@ -1,13 +1,25 @@
 package mobi.lab.componentsdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View itemBoundedView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        itemBoundedView = findViewById(R.id.item_bounded_view);
+        itemBoundedView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BoundedViewActivity.class));
+            }
+        });
     }
 }
