012328dfada9665dddc4f3192885030ea3c471bd
lauris <lauris.kruusamae@lab.mobi>
2023-10-03 13:44:01 +0300

    Add button component page to catalog and an initial version of filled and tonal buttons

---
+++ app-demo/src/main/java/mobi/lab/components/demo/presentation/button/ButtonFragment.kt
@@ -0,0 +1,26 @@
+package mobi.lab.components.demo.presentation.button
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import mobi.lab.components.button.LabButton
+import mobi.lab.components.demo.databinding.FragmentButtonBinding
+import mobi.lab.components.demo.util.FragmentBindingHolder
+import mobi.lab.components.demo.util.ViewBindingHolder
+
+class ButtonFragment : Fragment(), ViewBindingHolder<FragmentButtonBinding> by FragmentBindingHolder() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return createBinding(FragmentButtonBinding.inflate(inflater, container, false), this) {
+            switchEnabled.setOnCheckedChangeListener { _, isChecked ->
+                loopButtons { isEnabled = isChecked }
+            }
+        }
+    }
+
+    private fun FragmentButtonBinding.loopButtons(action: LabButton.() -> Unit) {
+        listOf(filled, filledBig, tonal, tonalBig).map(action)
+    }
+}
