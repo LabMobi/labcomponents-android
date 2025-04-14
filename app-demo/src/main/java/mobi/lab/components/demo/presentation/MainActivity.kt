4e18cdce44f7c7d5b100809d1068f0e9434c4f5c
Harri Kirik <harri.kirik@lab.mobi>
2025-04-14 11:26:42 +0300

    MW-95: Revert the debug option

--- app-demo/src/main/java/mobi/lab/components/demo/presentation/MainActivity.kt
+++ app-demo/src/main/java/mobi/lab/components/demo/presentation/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menu.add(0, MENU_ITEM_SWITCH_NIGHT_MODE, 0, "Switch UI mode")
         // Uncomment this to open MaterialThemeActivity to validate components with a regular Material theme
-        menu.add(0, MENU_ITEM_SWITCH_MATERIAL_THEME, 1, "Open Material Activity")
+//        menu.add(0, MENU_ITEM_SWITCH_MATERIAL_THEME, 1, "Open Material Activity")
         return true
     }
 
